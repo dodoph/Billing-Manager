2ad3f17d49b1d80b4033f235e245784009fccc78
Jessie Tang <tangqiaojie@hotmail.com>
2019-03-04 10:11:31 -0800

    update

--- code/public/filterByCategory.js
+++ code/public/filterByCategory.js
@@ -2,6 +2,6 @@ function filterByCategory() {
 
     var category  = document.getElementById('event_filter').value
     //construct the URL and redirect to it
-    window.location = '/home/filter/' + encodeURI(category)
+    window.location = '/home/filter/' + id + '?categoryname=' + encodeURI(category)
 }
 
