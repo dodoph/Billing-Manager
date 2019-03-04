35e9e51ec64e92d92e3176e99d76873ac6d44022
Jessie Tang <tangqiaojie@hotmail.com>
2019-03-04 10:12:34 -0800

    update

--- code/public/searchEvent.js
+++ code/public/searchEvent.js
@@ -1,6 +1,6 @@
-function searchEvent() {
-
-    var event_search_string  = document.getElementById('event_search_string').value
+function searchEvent(id) {
+    var event_search_string  = document.getElementById('eventNameSearch').value
     //construct the URL and redirect to it
-    window.location = '/home/search/' + encodeURI(event_search_string)
+    window.location = '/home/search/' + id  + '?eventname=' + encodeURI(event_search_string)
+
 }
